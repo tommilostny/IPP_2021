bc560752f1b4b2b431d29ec1e8a458345091bf11
Tom Milostny <tommilostny@live.com>
2021-03-30 22:36:16 +0200

    Added list of opcodes, commented until implemented

--- interpret/opcodes.py
+++ interpret/opcodes.py
@@ -5,5 +5,54 @@ def decode_opcode(instr_element:Element) -> Instruction:
         return OPCODES.get(instr_element.attrib["opcode"], Instruction)(instr_element)
 
 OPCODES = {
+    #Práce s rámci, volání funkcí
+    #"MOVE" : Move,
+    #"CREATEFRAME" : CreateFrame,
+    #"PUSHFRAME" : PushFrame,
+    #"POPFRAME" : PopFrame,
+    #"DEFVAR" : Defvar,
+    #"CALL" : Call,
+    #"RETURN" : Return,
+
+    #Práce s datovým zásobníkem
+    #"PUSHS" : Pushs,
+    #"POPS" : Pops,
+
+    #Aritmetické, relační, booleovské a konverzní instrukce
+    #"ADD" : Add,
+    #"SUB" : Sub,
+    #"MUL" : Mul,
+    #"IDIV" : Idiv,
+    #"LT" : Lt,
+    #"GT" : Gt,
+    #"EQ" : Eq,
+    #"AND" : And,
+    #"OR" : Or,
+    #"NOT" : Not,
+    #"INT2CHAR" : Int2Char,
+    #"STRI2INT" : Stri2Int,
+
+    #Vstupně-výstupní instrukce
+    #"READ" : Read,
     "WRITE" : Write,
+
+    #Práce s řetězci
+    #"CONCAT" : Concat,
+    #"STRLEN" : Strlen,
+    #"GETCHAR" : Getchar,
+    #"SETCHAR" : Setchar,
+
+    #Práce s typy
+    #"TYPE" : Type,
+
+    #Instrukce pro řízení toku programu
+    #"LABEL" : Label,
+    #"JUMP" : Jump,
+    #"JUMPIFEQ" : JumpIfEq,
+    #"JUMPIFNEQ" : JumpIfNeq,
+    #"EXIT" : Exit,
+
+    #Ladicí instrukce
+    #"DPRINT" : Dprint,
+    #"BREAK" :Break 
 }
