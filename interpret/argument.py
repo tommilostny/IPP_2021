fdd73888b609d5046ba96d3054b663f6b68a366a
Tom Milostny <tommilostny@live.com>
2021-03-31 00:13:47 +0200

    Added argument type matching

--- interpret/argument.py
+++ interpret/argument.py
@@ -4,7 +4,27 @@ class Argument:
     def __init__(self, element:Element):
         try:
             self.type = str(element.attrib["type"])
+
+            if self.type == "int":
+                self.value = int(element.text)
+
+            elif self.type == "string":
+                self.value = parse_string_arg(element.text)
+
+            elif self.type == "bool":
+                if element.text == "true": self.value = True
+                elif element.text == "false": self.value = False
+                else: raise TypeError()
+            
+            elif self.type == "nil":
+                self.value = None
+                
         except Exception as e:
             raise Exception(f"{element.tag}: No attribute {e}, just {element.attrib}.")
 
-        self.value = element.text ##TODO: decode value based on type
+
+def parse_string_arg(string:str) -> str: #TODO
+    if string is None:
+        return ""
+    else:
+        return string
