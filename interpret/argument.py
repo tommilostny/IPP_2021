fb7cd9304da1c66fa6553a9ca7eab1b2d3714641
Tom Milostny <tommilostny@live.com>
2021-04-13 19:18:08 +0200

    Updated argument checks

--- interpret/argument.py
+++ interpret/argument.py
@@ -1,21 +1,34 @@
+import re
+from typing import List
 from xml.etree.ElementTree import Element
 
-class Argument:
-    def __init__(self, element:Element, syntax_symbol:str):
-        self.type = str(element.attrib["type"])
 
-        if syntax_symbol == "symb":
+class Argument:
+    def __init__(self, element:Element, syntax_symbols:List[str]):
+        try: self.type = str(element.attrib["type"])
+        except:
+            raise SyntaxError(f"Bad argument: missing type attribute: {element} {element.attrib}")
+
+        error_flag = len(element.attrib.keys()) > 1 or not re.compile("^arg\d$").match(element.tag)
+        if not error_flag:
+            self.order = int(element.tag[3]) - 1
+            error_flag = self.order < 0 or self.order >= len(syntax_symbols)
+
+        if error_flag:
+            raise SyntaxError(f"Bad argument: Invalid XML element tag: {element} {element.attrib}")
+
+        if syntax_symbols[self.order] == "symb":
 
             if self.type == "int":
                 self.value = int(element.text)
 
             elif self.type == "string":
-                self.value = parse_string_arg(element.text)
+                self.value = _parse_string_arg(element.text)
 
             elif self.type == "bool":
                 if element.text == "true": self.value = True
                 elif element.text == "false": self.value = False
-                else: raise TypeError(f"{element.text} is not a valid bool")
+                else: raise SyntaxError(f"{element.text} is not a valid bool")
             
             elif self.type == "nil":
                 self.value = None
@@ -23,36 +36,36 @@ class Argument:
             elif self.type == "var":
                 self.value = element.text
 
-            else: raise SyntaxError(f"{self.type} is not valid {syntax_symbol}")
+            else: raise SyntaxError(f"{self.type} is not valid {syntax_symbols[self.order]}")
         
-        elif syntax_symbol == "var" and self.type == "var":
+        elif syntax_symbols[self.order] == "var" and self.type == "var":
             self.value = element.text
 
-        elif syntax_symbol == "label" and self.type == "label":
+        elif syntax_symbols[self.order] == "label" and self.type == "label":
             self.value = element.text
 
-        elif syntax_symbol == "type" and self.type == "type" and element.text in ["int", "string", "bool"]:
+        elif syntax_symbols[self.order] == "type" and self.type == "type" and element.text in ["int", "string", "bool"]:
             self.value = element.text
 
-        else: raise SyntaxError(f"{self.type} {element.text} is not valid {syntax_symbol}")
+        else: raise SyntaxError(f"{self.type} {element.text} is not valid {syntax_symbols[self.order]}")
 
 
-def parse_string_arg(string:str) -> str:
+def _parse_string_arg(string:str) -> str:
     if string is None:
         return ""
-    else:
-        result = ""
-        loading_escape_sequence = False
-        for char in string:
-            if not loading_escape_sequence:
-                if char == "\\":
-                    loading_escape_sequence = True
-                    ascii = ""
-                else:
-                    result += char
+
+    result = ""
+    loading_escape_sequence = False
+    for char in string:
+        if not loading_escape_sequence:
+            if char == "\\":
+                loading_escape_sequence = True
+                ascii = ""
             else:
-                ascii += char
-                if len(ascii) == 3:
-                    result += chr(int(ascii))
-                    loading_escape_sequence = False
-        return result
+                result += char
+        else:
+            ascii += char
+            if len(ascii) == 3:
+                result += chr(int(ascii))
+                loading_escape_sequence = False
+    return result
--- interpret/instructions/instruction_base.py
+++ interpret/instructions/instruction_base.py
@@ -7,7 +7,6 @@ from argument import Argument
 class InstructionBase:
     def __init__(self, element:Element, syntax_symbols:List[str]):
         self.name = type(self).__name__.upper()
-        
         format_syntax_err = SyntaxError(f"{self.name}: Bad instruction element {element} {element.attrib} (syntax: {syntax_symbols})")
         
         try: self.order = int(element.attrib["order"])
@@ -18,14 +17,11 @@ class InstructionBase:
         if element.tag != "instruction" or len(element.attrib.keys()) != 2 or self.order < 1:
             raise format_syntax_err
 
-        self.arguments:List[Argument] = []
-
-        for argument in element:
-            self.arguments.append(Argument(argument, syntax_symbols[len(self.arguments)]))
+        self.arguments = _load_arguments(self, element, syntax_symbols)
 
 
     def invoke(self) -> Union[None, int]:
-        """Specific instruction implementation will be invoked here."""
+        """Specifická implementace instrukce bude volána zde."""
         ...
 
 
@@ -42,3 +38,21 @@ def _check_duplicate_order(new_instr:InstructionBase):
                 duplicate_instr = i.name
                 break
         raise SyntaxWarning(f"{new_instr.name}: Duplicate order {new_instr.order} with instruction {duplicate_instr}")
+
+
+def _load_arguments(new_instr:InstructionBase, element:Element, syntax_symbols:List[str]) -> List[Argument]:
+    args:List[Argument] = []
+
+    for arg in element:
+        try:
+            args.append(Argument(arg, syntax_symbols))
+        except SyntaxError as e:
+            raise SyntaxError(f"{new_instr.name} (order: {new_instr.order}): {e}")
+
+    args.sort(key=lambda x: x.order)
+
+    for i in range(0, len(syntax_symbols)):
+        if args[i].order != i:
+            raise SyntaxError(f"{new_instr.name} (order: {new_instr.order}): Instruction missing argument {i + 1} (syntax: {syntax_symbols})")
+
+    return args
